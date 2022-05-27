b1cf93aaa3b34e32ef298b17cd4fe16cf3e5aee4
RamiroSouzaMesmo <souzasramiro@gmail.com>
2022-05-26 21:25:16 -0300

    Conserto de menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,8 @@
 const btnMobile = document.getElementById('btn-mobile');
+const item1 = document.getElementById('sobre-menu');
+const item2 = document.getElementById('tecnologias-menu');
+const item3 = document.getElementById('portfolio-menu');
+const item4 = document.getElementById('contato-menu');
 
 function toggleMenu(event){
     if(event.type === 'touchstart') event.preventDefault(); 
@@ -14,6 +18,24 @@ function toggleMenu(event){
     }
 }
 
+function clickItemMenu(event){
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+    const active = nav.classList.contains('active')
+    event.currentTarget.setAttribute('aria-expanded', active);
+    if(active){
+        event.currentTarget.setAttribute('area-label', 'Fechar Menu');
+    }
+    else{
+        event.currentTarget.setAttribute('area-label', 'Abrir Menu');
+    }
+}
+
 
 btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
+
+item1.addEventListener('click', clickItemMenu);
+item2.addEventListener('click', clickItemMenu);
+item3.addEventListener('click', clickItemMenu);
+item4.addEventListener('click', clickItemMenu);
